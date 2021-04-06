a766d964a3ff7312b9ce45d402a452b9d44e49d7
Yang Yu Hsuan <yuhsuan@gmail.com>
2021-04-06 13:19:31 +0800

    dailys

---
+++ test/eventData.swift
@@ -0,0 +1,17 @@
+//
+//  eventData.swift
+//  test
+//
+//  Created by Alice Lai on 2021/3/30.
+//  Copyright © 2021 graduateproj. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class eventDataList : Object{
+    @objc dynamic var content = " "
+    @objc dynamic var note :String? = nil
+//    @objc dynamic var Starttime = Date()
+//    @objc dynamic var Endtime = Date()
+}
